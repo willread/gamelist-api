56abbed2522fc9528080e0cdff9e02ae26d7a4dc
Will Read <will@willread.ca>
2020-03-30 13:26:48 -0600

    Modernize things a bit

--- api.js
+++ api.js
@@ -1,24 +1,19 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var mongoSanitize = require('mongo-sanitize');
-var mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoSanitize = require('mongo-sanitize');
+const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
-var app = express();
+const app = express();
 
 // Set up bodyParser and set a sane upper size limit
 
 app.use(bodyParser.json({limit: '5mb'}));
 
-// Allow validating input
-
-app.use(expressValidator());
-
 // Allow cross-origin
 
-app.all('*', function(req, res, next) {
+app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin); // Allow all origins
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
@@ -28,26 +23,26 @@ app.all('*', function(req, res, next) {
 
 // Protect against mongo query attacks
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   req.body = mongoSanitize(req.body);
   next();
 });
 
 // Initialize the app.
 
-var db = mongoose.connection;
-db.on('error', function() {
+const db = mongoose.connection;
+db.on('error', () => {
   process.exit(1);
 });
 
-db.once('open', function() {
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
+db.once('open', () => {
+  const server = app.listen(process.env.PORT || 8080, function () {
+    const port = server.address().port;
     console.log('App now running on port', port);
   });
 });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.end('');
 });
 
--- games.js
+++ games.js
@@ -1,12 +1,13 @@
-var express = require('express');
+const express = require('express');
+const fetch = require('isomorphic-unfetch');
 
 // Configure router
 
-var router = express.Router();
+const router = express.Router();
 
 // Search for games
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     const query = req.query.query; // TODO: Sanitize query
     const url = `https://www.giantbomb.com/api/search?api_key=${process.env.GIANTBOMB_API_KEY}&format=json&resources=game&query=${query}&limit=5&field_list=id,name,image,platforms`;
     const results = await (await fetch(url)).json();
